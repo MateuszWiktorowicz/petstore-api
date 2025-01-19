c0fc8034b05e2b855c72e47dda59c381d89f8c07
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2025-01-19 16:39:02 +0100

    add CreatePetRequest to validate input data

---
+++ app/Http/Requests/CreatePetRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'nullable|integer',
+            'name' => 'required|string|max:255',
+            'category' => 'required|string|max:255',
+            'photoUrls' => 'required|string',
+            'tags' => 'required|string',
+            'status' => 'required|string|in:available,pending,sold',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'category.required' => 'The category field is required.',
+            'photoUrls.required' => 'Please provide at least one photo URL.',
+            'tags.required' => 'Please provide at least one tag.',
+            'status.in' => 'The status must be one of the following: available, pending, or sold.',
+        ];
+    }
+}
