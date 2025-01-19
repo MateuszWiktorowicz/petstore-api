2888c2c58bfa0c1127d553a1f49f0cfafefcaaf2
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2025-01-19 16:29:39 +0100

    add route for getPetById

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pet', [PetController::class, 'getPetById'])->name('petstore.getPet');
